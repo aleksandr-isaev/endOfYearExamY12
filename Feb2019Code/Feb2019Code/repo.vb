257983e1ea8e96e94b72c2100c0856b3dfe98ede
Roshni0 <Roshni.Vachhani@tts.edu.sg>
2019-01-25 09:29:47 +0800

    random called number

--- Feb2019Code/Feb2019Code/repo.vb
+++ Feb2019Code/Feb2019Code/repo.vb
@@ -1,5 +1,6 @@
 ﻿Public Class repo
     Public Shared Function NewRandom(ByVal lowerbound As Integer, ByVal upperbound As Integer)
+        Randomize()
         Return CInt(Math.Floor((upperbound - lowerbound + 1) * Rnd())) + lowerbound
 
     End Function
