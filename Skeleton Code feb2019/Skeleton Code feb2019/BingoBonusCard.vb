8e02b2c565c71afec1934a4c573594a9e5b08940
Bnejamo <btleighty8@gmail.com>
2019-04-22 10:43:52 +0800

    One line Check for bonus game
    added a way to check if one line of the bingo card is full

--- Skeleton Code feb2019/Skeleton Code feb2019/BingoBonusCard.vb
+++ Skeleton Code feb2019/Skeleton Code feb2019/BingoBonusCard.vb
@@ -7,9 +7,30 @@
         If matched = 15 Then
             Return 1 ' if all numbers matched then returns 1
         End If
-
         Console.WriteLine("You only matched " & matched)
+        For x = 0 To 2
+            matched = 0
+            For y = 0 To 8
+                If numbers(x, y) <> 0 Then
+                    For z = 0 To tail ' 0 to amount of umbers called
+                        If numbers(x, y) = calledNumbers(z) Then
+                            matched += 1 ' checks if numbers matched with calledNUmbers
+                        End If
+                    Next
+
+                End If
+
+            Next
+
+
+        Next
+
+
+
         Return 0 ' if no numbers matched returns 0
+
+
+
     End Function
 
     Private Function FullHouse(ByVal calledNumbers As Integer(), ByVal tail As Integer) As Integer
@@ -27,6 +48,8 @@
             Next
         Next
         Return matched
+        'new loop to check if the user has a full row 
+
     End Function
 
 
