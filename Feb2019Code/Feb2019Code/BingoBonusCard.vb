7d59bffb48b802007636fce925a433fb7dae5a90
aleksandr-isaev <a.isaev101@gmail.com>
2019-04-22 09:52:14 +0800

    Added Structure

--- Feb2019Code/Feb2019Code/BingoBonusCard.vb
+++ Feb2019Code/Feb2019Code/BingoBonusCard.vb
@@ -1,9 +1,9 @@
 ﻿Public Class BingoBonusCard
     Inherits BingoCard
-
     Public Overrides Function GameOver(ByVal calledNumbers As Integer(), ByVal tail As Integer) As Integer
         Dim matched As Integer
         matched = FullHouse(calledNumbers)
+
         If matched = 15 Then
             Return 1
         ElseIf Corners(calledNumbers) Then
@@ -19,45 +19,65 @@
     'Changed so that it checks for -1's
     Private Function FullHouse(ByVal calledNumbers As Integer()) As Integer
         Dim matched As Integer
+
         For x = 0 To 2
+
             For y = 0 To 8
+
                 If numbers(x, y) = -1 Then
                     matched += 1
                 End If
+
             Next
+
         Next
         Return matched
     End Function
     'Checks the 4 corners
     Private Function Corners(ByVal calledNumbers As Integer()) As Boolean
         Dim matched As Integer
+
         For x = 0 To 2
+
             For y = 0 To 8
+
                 If numbers(x, y) = -1 Then
+
                     If (x = 0 And y = 0) Or (x = 0 And y = 8) Or (x = 2 And y = 0) Or (x = 2 And y = 8) Then
                         matched += 1
                     End If
+
                 End If
+
             Next
+
         Next
+
         If matched = 4 Then
             Return True
         Else
             Return False
         End If
+
     End Function
     'Checks horizontal lines
     Private Function Horizontal(ByVal calledNumbers As Integer()) As Boolean
         Dim matched() As Integer = {0, 0, 0}
+
         For x = 0 To 2
+
             For y = 0 To 8
+
                 If numbers(x, y) = -1 Then
                     matched(x) += 1
                 End If
+
             Next
+
             If matched(x) = 5 Then
                 Return True
             End If
+
         Next
         Return False
     End Function
--- Feb2019Code/Feb2019Code/BingoCard.vb
+++ Feb2019Code/Feb2019Code/BingoCard.vb
@@ -1,6 +1,5 @@
 ﻿Public Class BingoCard
     Protected numbers(2, 8) As Integer
-
     Public Sub New()
         numbers = AssignNumbers()
     End Sub
@@ -28,63 +27,82 @@
 
         For x = 0 To 4
             Dim base As Integer
+
             If cardnumbers(1, row3(x)) = 0 Then
 
                 base = (cardnumbers(0, row3(x)) Mod 10) + 1
             Else
                 base = (cardnumbers(1, row3(x)) Mod 10) + 1
             End If
+
             base = repo.NewRandom(base, 10)
             cardnumbers(2, row3(x)) = base + (10 * row3(x))
         Next
-        Return cardnumbers
 
+        Return cardnumbers
     End Function
 
     Public Sub Displaycard()
         For x = 0 To 2
+
             Console.Write("|")
             For y = 0 To 8
+
                 ' Console.Write(numbers(x, y) & ",")
+
                 If Len(CStr(numbers(x, y))) = 2 Then 'this if statement is for spacing and alignment of the board
+
                     If numbers(x, y) = -1 Then 'added this nested IF statement in order to display X when the number is picked
                         Console.Write(" X | ")
                     Else
                         Console.Write(numbers(x, y) & " | ") 'changed the , to a | so it is easier to see the columns separately
                     End If
+
                 ElseIf Len(CStr((numbers(x, y)))) = 1 Then
                     Console.Write(" " & numbers(x, y) & " | ")
                 End If
+
             Next y
             Console.WriteLine()
+
         Next x
     End Sub
     Public Sub removenumbers(ByVal callernum As Integer) 'added sub for when number is picked
         For x = 0 To 2
+
             For y = 0 To 8
+
                 If numbers(x, y) = callernum Then
+
                     numbers(x, y) = -1
                 End If
+
             Next
+
         Next
     End Sub
     Private Function AssignRowPlaces() As Integer()
         Dim numberCount As Integer
         Dim row(4) As Integer
         Dim match As Boolean
+
         For x = 0 To 4
+
             row(x) = -1
+
         Next
         Do While numberCount <= 4
+
             match = False
             row(numberCount) = repo.NewRandom(0, 8)
             numberCount += 1
 
             For x = 0 To 4
-                If row(numberCount - 1) = row(x) And numberCount - 1 <> x Then
 
+                If row(numberCount - 1) = row(x) And numberCount - 1 <> x Then
                     match = True
                 End If
+
             Next
 
             If match Then
@@ -92,20 +110,28 @@
             End If
 
         Loop
+
         row = rearrage(row, 4)
         Return row
     End Function
 
     Private Function rearrage(ByVal dataSet As Integer(), ByVal size As Integer) As Integer()
         Dim i, j As Integer
+
         For i = 0 To size - 1
+
             For j = 0 To size - 1
+
                 If (dataSet(j) > dataSet(j + 1)) Then
+
                     Dim temp As Integer = dataSet(j)
                     dataSet(j) = dataSet(j + 1)
                     dataSet(j + 1) = temp
+
                 End If
+
             Next
+
         Next
         Return dataSet
     End Function
@@ -113,17 +139,25 @@
 
     Public Overridable Function GameOver(ByVal calledNumbers As Integer(), ByVal tail As Integer) As Integer
         Dim matched As Integer
+
         For x = 0 To 2
+
             For y = 0 To 8
+
                 If numbers(x, y) <> 0 Then
 
                     For z = 0 To tail
+
                         If numbers(x, y) = calledNumbers(z) Then
                             matched += 1
                         End If
+
                     Next
+
                 End If
+
             Next
+
         Next
         Return matched
     End Function
